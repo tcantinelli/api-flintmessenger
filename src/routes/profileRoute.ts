12b011f757f0f2430e4740a3b83e2724600a03fd
Thomas Cantinelli <tcantinelli@gmail.com>
2020-09-05 23:35:41 +0200

    Profile Delete

--- src/routes/profileRoute.ts
+++ src/routes/profileRoute.ts
@@ -3,6 +3,7 @@ import { Profile } from '../models/profiles';
 
 const router = Router();
 
+/* GET */
 router.get('/:profileId', (req: Request, res: Response) => {
 	const { profileId } = req.params;
 
@@ -14,24 +15,31 @@ router.get('/:profileId', (req: Request, res: Response) => {
 	});
 });
 
+/* DELETE */
+router.delete('/:profileId', (req: Request, res: Response) => {
+	const { profileId } = req.params;
+
+	Profile.findByIdAndDelete(profileId, (err, profile) => {
+		if (err) res.status(500).send("Il y a eu une erreur serveur");
+		if (profile == null) { res.status(404).send("Profile inconnu"); return; }
+		res.status(200).send('Le profile a été supprimé');
+	});
+});
+
+/* CREATE */
 router.post('/', (req: Request, res: Response) => {
 	const { email, firstname, lastname } = req.body;
 
 	if (email && firstname && lastname) {
 		const newProfile = new Profile({ email: email, firstname: firstname, lastname: lastname })
 
-		newProfile.save()
-			.then(newProfileSaved => {
-				res.status(201).send({
-					id: newProfileSaved._id,
-					email, firstname, lastname
-				});
-			})
-			.catch(err => {
-				console.log(err);
-				res.status(500).send('Erreur serveur');
-			})
-
+		newProfile.save((err, profile) => {
+			if (err) res.status(500).send('Erreur serveur');
+			res.status(201).send({
+				id: profile._id,
+				email, firstname, lastname
+			});
+		});
 	} else {
 		res.status(400).send('Données manquantes');
 	}
