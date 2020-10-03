5dfedd11e102f3ee16554161998126f9122370c5
Thomas Cantinelli <tcantinelli@gmail.com>
2020-10-03 22:37:57 +0200

    test6

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,31 +1,32 @@
 module.exports = {
-  apps : [{
-    name: 'FlintAPI',
-    script: './dist/server.js',
+	apps: [{
+		name: 'FlintAPI',
+		script: './dist/server.js',
 
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    args: 'one two',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'development',
-	mongo_pass: 'Vd46IDLzXrwXQvfj'
-    },
-    env_production: {
-      NODE_ENV: 'production'
-    }
-  }],
+		// Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+		args: 'one two',
+		instances: 1,
+		autorestart: true,
+		watch: false,
+		max_memory_restart: '1G',
+		env: {
+			NODE_ENV: 'development',
+			mongo_pass: 'Vd46IDLzXrwXQvfj'
+		},
+		env_production: {
+			NODE_ENV: 'production',
+			mongo_pass: 'Vd46IDLzXrwXQvfj'
+		}
+	}],
 
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-    }
-  }
+	deploy: {
+		production: {
+			user: 'node',
+			host: '212.83.163.1',
+			ref: 'origin/master',
+			repo: 'git@github.com:repo.git',
+			path: '/var/www/production',
+			'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
+		}
+	}
 };
